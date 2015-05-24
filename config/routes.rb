4a744d6a2166120a4ea5aa10cd96027fcd28cb6d
Noah Hines <hinesnoa@msu.edu>
2015-05-24 15:45:13 -0500

    Added square, single parameter for pie.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root 'application#home'
 
   get '/home', to: 'application#home'
-  
+
   get '/pie/:width/:height', to: 'application#pie'
+  get '/pie/:width', to: 'application#pie'
 
   get '/bar/:width/:height', to: 'application#bar'
 
