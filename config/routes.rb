2c630699e3e08dcff693e4a879a6020d50ca9539
Noah Hines <hinesnoa@msu.edu>
2015-05-24 14:27:20 -0500

    Added constraints for super large images.

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,7 @@ Rails.application.routes.draw do
   root 'application#home'
 
   get '/home', to: 'application#home'
-
-  #get '/bar/:id', to: 'application#bar', id: 3
-
-  #get '/bar/:tag', to: 'application#bar', as: :tagged_products
-
-  #match 'bar/:name', :to => 'application#bar'
-
+  
   get '/pie/:width/:height', to: 'application#pie'
 
   get '/bar/:width/:height', to: 'application#bar'
