30f3642eb87db2348376741cc1b0eab5e060c532
Noah Hines <hinesnoa@msu.edu>
2015-05-25 18:44:24 -0500

    Holy colors, Batman!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,21 +12,48 @@ class ApplicationController < ActionController::Base
 	 	@max_width = 5000 #default constraint is 5000x5000 image
 		@max_height = 5000
 
+		# Dimension Handling
+		# ---------------
+
 		# If height is not specified, assume square
 		@width = params[:width]
-		if !(5 < params[:height].to_i && params[:height].to_i <= 5000)
-			@height = @width
-		else
-			@height = params[:height]
+		if params[:height] != nil
+			if !(5 < params[:height].to_i && params[:height].to_i <= 5000)
+				@height = @width
+			else
+				@height = params[:height]
+			end
+			# Constraints added so the server doesn't get murdered
+			if (@width.to_i > @max_width || @height.to_i > @max_height)
+				return
+			end
+		else @height = @width
 		end
 
-		# -8 and -18 due to default css margins
+		# adjusted width and heights -8 and -18 due to default css margins
 		@width_a = (@width.to_i-8).to_s
 		@height_a = (@height.to_i-16).to_s
 
-		# Constraints added so the server doesn't get murdered
-		if (@width.to_i > @max_width || @height.to_i > @max_height)
-			return
+		# Color Handling
+		# ---------------
+		# If no color parameter given, make @color="" and use default
+		# Chartholdr color palette.
+		#
+		# example: params[:color] = c=ffffff
+		if params[:color] != nil
+			if (params[:color].length == 5 || params[:color].length == 7 || params[:color].length == 8)
+				@color = params[:color][2,params[:color].length-1]
+			else
+				@color = ""
+			end
+		else
+			if params[:height] != nil
+				if (params[:height].length == 5 || params[:height].length == 7 || params[:height].length == 8)
+					@color = params[:height][2,params[:height].length-1]
+				else
+					@color =""
+				end
+			end
 		end
 	end
 
@@ -54,11 +81,31 @@ class ApplicationController < ActionController::Base
 		render_it(@layout_type)
 	end
 
+	# Amount should be a decimal between 0 and 1. Lower means darker
+	def darken_color(hex_color, amount=0.4)
+	  hex_color = hex_color.gsub('#','')
+	  rgb = hex_color.scan(/../).map {|color| color.hex}
+	  rgb[0] = (rgb[0].to_i * amount).round
+	  rgb[1] = (rgb[1].to_i * amount).round
+	  rgb[2] = (rgb[2].to_i * amount).round
+	  "#%02x%02x%02x" % rgb
+	end
+
+	# Amount should be a decimal between 0 and 1. Higher means lighter
+	def lighten_color(hex_color, amount=0.6)
+	  hex_color = hex_color.gsub('#','')
+	  rgb = hex_color.scan(/../).map {|color| color.hex}
+	  rgb[0] = [(rgb[0].to_i + 255 * amount).round, 255].min
+	  rgb[1] = [(rgb[1].to_i + 255 * amount).round, 255].min
+	  rgb[2] = [(rgb[2].to_i + 255 * amount).round, 255].min
+	  "#%02x%02x%02x" % rgb
+	end
+
 	private
 		def render_it(layout = "pie") #default chart is pi
 			#TODO move this logic to generator class
 			#TODO add support for SVGs
-
+			
 			html = File.open("#{Rails.root}"+"/app/views/layouts/"+layout.to_s+"_layout.html.erb").read
 		    template = ERB.new(html)
 			template = template.result(get_binding).html_safe
@@ -73,7 +120,8 @@ class ApplicationController < ActionController::Base
 			@tempImageFile = Tempfile.new(['image', '.png'], "#{Rails.root}/tmp")
 
 			# data object is used to pass in parameters into the js file
-			data = {"width" => @width, "height" => @height, "template" => @templateFile.path, "image" => @tempImageFile.path}.to_json
+			data = {"width" => @width, "height" => @height,
+				"template" => @templateFile.path, "image" => @tempImageFile.path}.to_json
 			dataFile = Tempfile.new(['data', '.txt'], "#{Rails.root}/tmp")
 			dataFile.write(data)
 			File.chmod(444, dataFile.path)
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,18 @@ Rails.application.routes.draw do
 
   get '/home', to: 'application#home'
 
+  # :height could also be color. We must check if :height is a string of
+  # format "c=ffffff", where "ffffff" is a hex color value of length 3 5 or 6
   get '/pie/:width/:height', to: 'application#pie'
   get '/pie/:width', to: 'application#pie'
+  get '/pie/:width/:height/:color', to: 'application#pie'
 
   get '/bar/:width/:height', to: 'application#bar'
   get '/bar/:width', to: 'application#bar'
+  get '/bar/:width/:height/:color', to: 'application#bar'
 
   get '/line/:width/:height', to: 'application#line'
   get '/line/:width', to: 'application#line'
+  get '/line/:width/:height/:color', to: 'application#line'
 
 end
